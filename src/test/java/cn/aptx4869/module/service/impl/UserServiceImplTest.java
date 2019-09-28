bd029f96aa50871fb4f6816de8bb63bfe20f5fc4
yuki <aranoyuki@yahoo.com>
2019-09-28 18:56:09 +0800

    add page-helper

---
+++ src/test/java/cn/aptx4869/module/service/impl/UserServiceImplTest.java
@@ -0,0 +1,31 @@
+package cn.aptx4869.module.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @org.junit.jupiter.api.Test
+    void getUserList() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertUsers() {
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getUserList1() {
+    }
+
+    @org.junit.Test
+    public void insertUsers1() {
+    }
+}
